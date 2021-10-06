7445c25be598da5fdec05063fa4b0b0d8a61577e
hdshea <hdshea@comcast.net>
2021-10-06 16:11:05 -0400

    package documentation start

---
+++ R/MIMIC4db-package.R
@@ -0,0 +1,7 @@
+#' @description
+#' MIMIC4db provides a tightly bound set of routines to reference the Google BigQuery version of
+#' the [MIMIC-IV v1.0](https://physionet.org/content/mimiciii/1.0/) database using base access
+#' routines from the [DBI](https://github.com/r-dbi/DBI) R package with an appropriate
+#' [bigrquery](https://github.com/r-dbi/bigrquery) DBIConnection.
+#'
+"_PACKAGE"
